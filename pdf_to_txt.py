ee7bb21813acdb5df4050fbb6ef29c4e25646f3c
karlita9 <karlanatalyrobles@gmail.com>
2021-11-26 18:33:18 -0600

    first commit

---
+++ pdf_to_txt.py
@@ -0,0 +1,20 @@
+import PyPDF2
+import os
+import sys
+
+def pdf_translator(pdf_file):
+
+
+    pdf_file_obj = open(pdf_file, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+
+    tot_pages = pdf_reader.getNumPages()
+        
+    title_page = pdf_reader.getPage(0)
+    text = title_page.extractText()
+
+    print("File: ", pdf_file)
+    print("Contains ", tot_pages, " pages")
+    print(text)
+
+pdf_translator(sys.argv[1])
